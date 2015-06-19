136e7cd568f8ec45c8c7c94de6d584cc1dc7efc4
Bruno Castro <miroldols@gmail.com>
2015-06-19 01:31:16 -0300

    - Criando a tabela tags  - Criando a tabela de ligaçÎáÎ÷Îýo tags com posts  - Fazendo relacionamento many to many  - RELACIONAMENTO MODELS - Criando Model e Migrations Many To Many

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,13 +4,14 @@
     <h1>Blog</h1>
 
     @foreach($posts as $post)
-        <h2>{{ $post->title }} <i>({{ $post->created_at }})</i></h2>
+        <h2>{{ $post->title }}</h2> <i>({{ $post->created_at }})</i>
         <p>{{ $post->content }}</p>
 
         <h3>Comments</h3>
         @foreach($post->comments as $comment)
             <b>Name: </b> {{ $comment->name }}<br />
-            <b>Comment: </b> {{ $comment->comment }}
+            <b>Comment: </b> {{ $comment->comment }}<br />
+            <b>Email: </b> {{ $comment->email }}<br />
         @endforeach
         <hr>
     @endforeach
