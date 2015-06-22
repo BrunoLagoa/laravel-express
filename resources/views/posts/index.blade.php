0dd16eb95673abab3585128e932ceab56d7244e2
Bruno Lagoa <miroldols@gmail.com>
2015-06-22 18:04:35 -0300

    - Listando as Tags no posts/index.blade.php  - RELACIONANDO MODELS - Listando utilizando relacionamentos

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,7 +6,12 @@
     @foreach($posts as $post)
         <h2>{{ $post->title }}</h2> <i>({{ $post->created_at }})</i>
         <p>{{ $post->content }}</p>
-
+        <b>Tags</b>
+        <ul>
+            @foreach($post->tags as $tag)
+            <li>{{ $tag->name }}</li>
+            @endforeach
+        </ul>
         <h3>Comments</h3>
         @foreach($post->comments as $comment)
             <b>Name: </b> {{ $comment->name }}<br />
